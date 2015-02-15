dbe6c486cc683ed38cd375c08c86b2b0163a27b5
Kyle Hasty <khasty720@gmail.com>
2015-02-15 09:46:10 -0500

    Added User Price to order_items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,8 +2,13 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
+
+    #Asign Correct Price
+    @order_item.unit_price = @order_item.set_unit_price(order_item_params[:product_id], current_user)
+
     @order.save
     session[:order_id] = @order.id
+
   end
 
   def update
@@ -19,9 +24,9 @@ class OrderItemsController < ApplicationController
     @order_item.destroy
     @order_items = @order.order_items
   end
-  
+
   private
   def order_item_params
-    params.require(:order_item).permit(:quantity, :product_id)
+    params.require(:order_item).permit(:quantity, :unit_price, :product_id)
   end
 end
