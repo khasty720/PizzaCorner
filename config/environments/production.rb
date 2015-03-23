aa78b8aacddd80263c1838a164f2f045fda899cb
Kyle Hasty <khasty720@gmail.com>
2015-03-23 18:23:00 -0400

    Prod SMTP Gmail Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   #Allow Helper for states
   config.action_controller.include_all_helpers = true
+
+  #------- Gmail Config --------
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'http://cameochem.herokuapp.com/',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  end
 end
